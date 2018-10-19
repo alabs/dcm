c182c01435fdb7949a61cc20c577affc8d457ca8
Mijail Rondon <mijail@riseup.net>
2018-10-19 13:58:31 +0200

    Add public uploads path

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/ruby-data/app"
 set :rbenv_type, :user
 
 append :linked_files, "config/application.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 set :puma_bind, "tcp://0.0.0.0:3000"
 set :puma_user, fetch(:user)
